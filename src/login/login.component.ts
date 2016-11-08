c187803c27e1d3a4bab2b3fd98b140dbccfca8da
Lior Gerstein <lior.gerstein@mac-liorge.local>
2016-11-08 16:32:49 +0200

    added forms to the login.component

--- src/login/login.component.ts
+++ src/login/login.component.ts
@@ -2,20 +2,42 @@
  * Created by lior.gerstein on 08/11/2016.
  */
 import {Component} from "@angular/core";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
-
+/**
+ * Form Example with NgForms.
+ */
 @Component({
     selector: 'login',
     template: `
-        <form>
-            <input type="text" placeholder="User Name">
-            <input type="password" placeholder="Password">
+        <form [formGroup]="login" (ngSubmit)="send()">
+            <input name="username" formControlName="username"  type="text" placeholder="User Name"/>
+            <input name="password" formControlName="password"  type="password"  placeholder="Password"/>
             <button>Login</button>
         </form>
     `
 })
 export class LoginComponent {
 
+    private login: FormGroup;
+    private  username: FormControl;
+    private password: FormControl;
+
+    constructor() {
+
+        this.username = new FormControl('', Validators.required);
+        this.password = new FormControl();
+
+        this.login = new FormGroup({
+            username: this.username, // attaching username variable to the formControlName attribute
+            password: this.password // attaching password variable to the formControlName attribute
+        })
+    }
+
+    send(){
+        console.log(this.login.value)
+        console.log(this.login.valid)
+    }
 }
 
 
--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -1,13 +1,14 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {LoginComponent} from "./login.component";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 /**
  * Created by lior.gerstein on 08/11/2016.
  */
 
 @NgModule({
   declarations: [LoginComponent],
-    imports: [BrowserModule],
+    imports: [BrowserModule, ReactiveFormsModule, FormsModule],
     exports: [LoginComponent]
 
 })
